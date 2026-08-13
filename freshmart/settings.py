5a45526a077334c6ab78beb23e29e904bb95943d
vignesh mullangi <vigneshmullangi@gmail.com>
2026-08-13 18:38:22 +0530

    Fix static files configuration

--- freshmart/settings.py
+++ freshmart/settings.py
@@ -106,8 +106,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # ─────────────────────────────────────────
 # LOGIN / REDIRECT
